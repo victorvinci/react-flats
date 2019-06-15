367ab1a5cbb5670ebcf1bcbfaad704349328f2f8
victor vinci <victorvinci@protonmail.com>
2019-06-15 21:22:09 +0200

    Updates

---
+++ precache-manifest.283cd49cf2872dd13c15beb226db6341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e56b176fc8f232d121c1a840dda2a42",
+    "url": "/react-flats/index.html"
+  },
+  {
+    "revision": "3763937dec8dad1e792e",
+    "url": "/react-flats/static/css/main.440b0bd3.chunk.css"
+  },
+  {
+    "revision": "b7389e5da7f7e5a4e3c3",
+    "url": "/react-flats/static/js/2.838d754f.chunk.js"
+  },
+  {
+    "revision": "3763937dec8dad1e792e",
+    "url": "/react-flats/static/js/main.bdb34d45.chunk.js"
+  },
+  {
+    "revision": "e6c1049c7e6c7834cab1",
+    "url": "/react-flats/static/js/runtime~main.ceb603fd.js"
+  }
+]);
